f5c263b7a4773f133722c8f5cf563d373a4a7d9b
komiyamma <komiyama0629@gmail.com>
2022-09-18 18:52:46 +0900

    初版相当

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,12 @@
-/// <reference path="../types/hm_jsmode_strict.d.ts" />
+/// <reference path="../types/hm_jsmode.d.ts" />
 
  　getconfig("CharSpace")
 
 var p = hidemaru.runProcess("a", "b", "c", "d");
 p.stdOut.onReadSeparated(function(out3){
     console.log(out3);
+    console.log()
     return 3;
 }, 0);
 
-
+hidemaruGlobal.message()
