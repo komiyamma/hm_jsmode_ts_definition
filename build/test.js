74235df72682858a3f38a260e7beb407d2889395
komiyamma <komiyama0629@gmail.com>
2022-09-19 19:12:22 +0900

    コミット

--- build/test.js
+++ build/test.js
@@ -1,14 +1,7 @@
 /// <reference path="../types/hm_jsmode.d.ts" />
-getconfig("CharSpace");
-var p = hidemaru.runProcess("a", "b", "c", "d");
-p.stdOut.onReadSeparated(function (out3) {
-    console.log(out3);
-    console.log("\r\n");
-    return 3;
-}, 0);
-hidemaruGlobal.message("OK");
-var processID = hidemaru.runProcess("abc", "def", "gui", "utf");
-processID.stdOut.onReadAll(function (out) {
-    console.log(out);
-});
-insert("あいうえお", 0, 0x0001);
+var _a = myfunc("あいうえお"), a = _a[0], b = _a[1];
+function myfunc(msg) {
+    debuginfo(2);
+    console.log(msg);
+    return [x(), msg.length];
+}
--- src/test.ts
+++ src/test.ts
@@ -1,28 +1,8 @@
 /// <reference path="../types/hm_jsmode.d.ts" />
 
- 　let ret = hideamruGlobal.getconfig("BoldFace");
-
-    hideamruGlobal.config("xActiveKakko:abc");
-
-var p = hidemaru.runProcess("a", "b", "c", "d");
-p.stdOut.onReadSeparated(function(out3){
-    console.log(out3);
-    console.log("\r\n");
-    return 3;
-}, 0);
-
-hideamruGlobal.message("OK");
-
-var processID = hidemaru.runProcess("abc", "def", "gui", "utf");
-processID.stdOut.onReadAll((out: string)=>{
-    console.log(out);
-})
-
-hideamruGlobal.insert("あいうえお", 0, 0x0001);
-
-message("OK");
-
-var b = gettotaltext();
-if (b.endsWith());
-
-var c = createobject("a");
+let [a, b]: number[] = myfunc("あいうえお");
+function myfunc(msg: string): number[] {
+    debuginfo(2);
+    console.log(msg);
+    return [x(), msg.length];
+}
