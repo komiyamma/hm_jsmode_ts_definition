f0e67a97650aee0e5f0a878f2ba40473953a742b
komiyamma <komiyama0629@gmail.com>
2023-04-29 14:09:46 +0900

    コミット

--- build/test.js
+++ build/test.js
@@ -1,27 +1,3 @@
 /// <reference path="../types/hm_jsmode.d.ts" />
-var len = myfunc("あいうえお");
-// コメント
-function myfunc(msg) {
-    debuginfo(2);
-    console.log(msg);
-    return msg.length;
-}
-var v = hidemaruversion();
-var arr = hidemaru.getCursorPos("wcs");
-var info = hidemaru.runProcess("abc", "abc", "guiStdio", "utf8");
-info.kill;
-var bb = hidemaru.getCursorPosFromMousePos("gcu", 100, 50);
-var c = hidemaruGlobal.browserpaneurl(2);
-var j = hidemaruGlobal.getselectedrange({ unit: "char" });
-var s = hidemaruGlobal.setselectionrange({ unit: "char", "items": [{ col1: 10, line1: 10 }], col1: 3, col2: 5, line1: 10, line2: 20 });
-var b = hidemaru.getCursorPosFromMousePos();
-var d = hidemaru.getTotalText();
-var num = hidemaru.setInterval(function () { }, 3);
-hidemaru.clearInterval(num);
-var json3 = {
-    clear: 1
-};
-var ret = getconfig({"*":{}});
-
-
-
+debuginfo(2);
+console.log("hello");
--- src/test.ts
+++ src/test.ts
@@ -1,36 +1,4 @@
 /// <reference path="../types/hm_jsmode.d.ts" />
 
-hidemaruGlobal
-hidemaru
-
-// クリップボードの内容を取得する hidemaruGlobal.*** の関数を使って
-function getClipboarText() {
-    let b = hidemaruGlobal.getclipboard();
-    if (b) {
-        return b;
-    }
-    return "";
-}
-
-let xp1: number = xpixel2();
-let xp2: number = ypixel2();
-
-
-renderpanecommand("top")
-
-let objFormat = {
-         items:[{
-           layer:"*",
-           items:[
-             {line1:"*",col1:"*",line2:"*",col2:"*",},
-           ]
-         }]
-       };
-
-let b:IConfigRGBHex = "#124";
-
-
-let r = getselectedrange()
-if (r.items?.length > 0) {
-  
-}
+debuginfo(2);
+console.log("hello");
